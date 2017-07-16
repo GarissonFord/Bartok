466edd184bbadd8707bbe794920a5c14d062e665
Gary DiCiano <garydiciano4@gmail.com>
2017-07-16 14:00:34 -0400

    Imported everything from Prospector

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour {
+
+	public string    suit;
+	public int       rank;
+	public Color     color = Color.black;
+	public string    colS = "Black";  // or "Red"
+	
+	public List<GameObject> decoGOs = new List<GameObject>();
+	public List<GameObject> pipGOs = new List<GameObject>();
+	
+	public GameObject back;     // back of card;
+	public CardDefinition def;  // from DeckXML.xml		
+
+	//List of SpriteRenderer components of this object
+	public SpriteRenderer[] spriteRenderers;
+
+	void Start() {
+		SetSortOrder(0);
+	}
+	
+	// property
+	public bool faceUp {
+		get {
+			return (!back.activeSelf);
+		}		
+		set {
+			back.SetActive(!value);
+		}
+	}	
+
+	//If spriteRenderers is not yet defined, this does so
+	public void PopulateSpriteRenderers() {
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer> ();
+		}
+	}
+
+	//Sets sortingLayerName on all SR components
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers ();
+
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+			Debug.Log (tSLN);
+		}
+	}
+
+	public void SetSortOrder(int sOrd) {
+		PopulateSpriteRenderers ();
+
+		//White background of the card is on the bottom (sOrd)
+		//Then the pips, decorators, face and such (sOrd + 1)
+		//The back is on top when visible (sOrd + 2)
+
+		//Iterate through all spriteRenderers
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if (tSR.gameObject == this.gameObject) {
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+
+			switch (tSR.gameObject.name) {
+			case "back":
+				tSR.sortingOrder = sOrd + 2;
+
+				break;
+			case "face":
+			default:
+				tSR.sortingOrder = sOrd + 1;
+				break;
+			}
+		}
+	}
+
+	//Virtual methods can be overridden by subclass methods with the same name
+	virtual public void OnMouseUpAsButton() {
+		//print (name);
+	}
+
+} // class Card
+
+[System.Serializable]
+public class Decorator{
+	public string	type;			// For card pips, tyhpe = "pip"
+	public Vector3	loc;			// location of sprite on the card
+	public bool		flip = false;	//whether to flip vertically
+	public float 	scale = 1.0f;
+}
+
+[System.Serializable]
+public class CardDefinition{
+	public string	face;	//sprite to use for face cart
+	public int		rank;	// value from 1-13 (Ace-King)
+	public List<Decorator>	
+					pips = new List<Decorator>();  // Pips Used					
+} // Class CardDefinition
---
+++ Assets/__Scripts/Scoreboard.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour {
+	public static Scoreboard S;
+
+	public GameObject prefabFloatingScore;
+
+	public bool _____________________;
+	[SerializeField]
+	private int _score = 0;
+	public string _scoreString;
+	public GameObject canvas;
+
+	public int score {
+		get {
+			return(_score);
+		}
+		set {
+			_score = value;
+			_scoreString = Utils.AddCommasToNumber (_score);
+		}
+	}
+
+	public string scoreString {
+		get {
+			return(_scoreString);
+		}
+		set {
+			scoreString = value;
+		}
+	}
+
+	void Awake() {
+		S = this;
+		canvas = GameObject.Find ("Canvas");
+	}
+
+	public void FSCallback(FloatingScore fs) {
+		score += fs.score;
+	}
+		
+	//Instantiates a new FloatingScore, initializes it
+	//Returns a pointer to the created FloatingScore 
+	//to allow more freedoms like altering font sizes
+	public FloatingScore CreateFloatingScore(int amt, List<Vector3> pts) {
+		GameObject go = Instantiate (prefabFloatingScore) as GameObject;
+		go.transform.SetParent (canvas.transform, true);
+		//Vector3 fsPos = new Vector3 (0.5f, 130.0f, 0.0f);
+		//rt.localPosition = fsPos;
+		//rt.position = fsPos;
+		FloatingScore fs = go.GetComponent<FloatingScore> ();
+		fs.score = amt;
+		fs.reportFinishTo = this.gameObject;
+		fs.Init (pts);
+		return(fs);
+	}
+}
